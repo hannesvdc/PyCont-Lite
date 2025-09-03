54d76ec6c0a98d3808db00c35a20497b12d14bd4
Hannes Vandecasteele <hannesvdc@gmail.com>
2025-09-03 12:47:58 -0400

    Bug for one-dimensional systems - compute the eigenvalue manually.

--- src/pycont/Stability.py
+++ src/pycont/Stability.py
@@ -13,10 +13,15 @@ def rightmost_eig(G : Callable[[np.ndarray, float], np.ndarray],
                   sp : Dict) -> float:
     M = len(u)
     rdiff = sp["rdiff"]
-
     jacobian = _makeJacobianOperator(G, u, p, rdiff)
-    J = slg.LinearOperator((M,M), jacobian)
-    eig_vals = slg.eigs(J, k=1, which='LR', return_eigenvectors=False)
-    
-    rightmost_eigenvalue = eig_vals[0]
+
+    # Special case for one-dimensional state vectors - Arnoldi won't work
+    if M == 1:
+        rightmost_eigenvalue = jacobian(np.array([1.0]))
+    else:
+        J = slg.LinearOperator((M,M), jacobian)
+        eig_vals = slg.eigs(J, k=1, which='LR', return_eigenvectors=False)
+        rightmost_eigenvalue = eig_vals[0]
+
+    # Only return the real part
     return float(np.real(rightmost_eigenvalue))
