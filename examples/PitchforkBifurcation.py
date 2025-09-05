a01d56d0459f5332c483a464571a0059da296742
Hannes Vandecasteele <hannesvdc@gmail.com>
2025-09-05 13:39:28 -0400

    Increasing ds_max for the pitchfork bifurcation - more fun this way.

--- examples/PitchforkBifurcation.py
+++ examples/PitchforkBifurcation.py
@@ -3,14 +3,14 @@ import numpy as np
 import pycont
 
 def PitchforkTest():
-	G = lambda x, r: r*x - x**3
-	u0 = np.array([-2.0])
-	p0 = 4.0
+	G = lambda u, p: p*u - u**3
+	u0 = np.array([-3.0])
+	p0 = 9.0
 
-	ds_max = 0.001
+	ds_max = 0.01
 	ds_min = 1.e-6
 	ds = 0.001
-	n_steps = 10000
+	n_steps = 5000
 	solver_parameters = {"tolerance": 1e-10}
 	continuation_result = pycont.pseudoArclengthContinuation(G, u0, p0, ds_min, ds_max, ds, n_steps, solver_parameters=solver_parameters)
 
