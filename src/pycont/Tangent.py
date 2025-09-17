1bed462a2fb612ab7d916c8391f0f95b5fc42773
Hannes Vandecasteele <hannesvdc@gmail.com>
2025-09-17 13:41:30 +0200

    Removing Newton-Schur algorithm for fast tangent computations. Added Newton-Krylov backup for large L-GMRES Residuals.

--- src/pycont/Tangent.py
+++ src/pycont/Tangent.py
@@ -1,67 +1,40 @@
 import numpy as np
 import numpy.linalg as lg
 import scipy.sparse.linalg as slg
+import scipy.optimize as opt
 
 from typing import Callable, Dict
-
-def _newton_schur(G: Callable[[np.ndarray, float], np.ndarray],
-				  u : np.ndarray, 
-				  p : float, 
-				  prev_tangent : np.ndarray, 
-				  current_guess : np.ndarray,
-				  G_value : np.ndarray,
-				  Gp : np.ndarray,
-				  M : int,
-				  rdiff : float,
-				  tolerance : float):
-    print('Trying one iteration of Newton-Schur to improve tangent condition')
-    tp = current_guess[M]
-    cu = prev_tangent[0:M]
-    cp = prev_tangent[M]
-	
-    # Solve for t_u
-    Gu_matvec = lambda v: (G(u + rdiff * v, p) - G_value) / rdiff
-    J = slg.LinearOperator((M,M), Gu_matvec)
-    rhs = -Gp * tp
-    tu, _ = slg.lgmres(J, rhs, x0=current_guess[0:M], maxiter=min(M+2, 20), atol=tolerance, rtol=1e-6)
-	
-    # Evaluate phi and its derivative
-    phi = np.dot(cu, tu) + cp * tp - 1.0
-    if np.abs(tp) > 0.01:
-        y = tu / tp
-    else:
-        y, _ = slg.lgmres(J, -Gp, x0=tu/tp, maxiter=min(M+2, 20), atol=tolerance, rtol=1e-6)
-    phi_prime = -np.dot(cu, y) + cp
-	
-    # Update the guess for tp and tu
-    tp = tp - phi / phi_prime
-    tangent = np.append(tu, tp)
-	
-    # Return the normalized tangent
-    return np.sign(np.dot(tangent, prev_tangent)) * tangent / lg.norm(tangent)
 
 def computeTangent(G: Callable[[np.ndarray, float], np.ndarray],
 				   u : np.ndarray, 
 				   p : float, 
 				   prev_tangent : np.ndarray, 
-				   sp : Dict, 
-				   eps_reg=1e-5) -> np.ndarray:
+				   sp : Dict) -> np.ndarray:
     rdiff = sp["rdiff"]
     M = len(u)
 
     # Create the linear system and right-hand side
-    G_value = G(u, p)
-    Gp = (G(u, p + rdiff) - G_value) / rdiff
+    Gp = (G(u, p + rdiff) - G(u, p - rdiff)) / (2.0*rdiff)
     def matvec(v):
-        J = (G(u + rdiff * v[0:M], p) - G_value) / rdiff + eps_reg * v[0:M] + Gp * v[M]
-        eq_2 = np.dot(prev_tangent, v) + eps_reg * v[M]
+        norm_v = lg.norm(v[0:M])
+        J = Gp * v[M]
+        if norm_v != 0.:
+            eps = rdiff / norm_v
+            J += (G(u + eps * v[0:M], p) - G(u - eps * v[0:M], p)) / (2.0*eps)
+        eq_2 = np.dot(prev_tangent, v)
         return np.append(J, eq_2)
     sys = slg.LinearOperator((M+1, M+1), matvec)
     rhs = np.zeros(M+1); rhs[M] = 1.0
 
 	# Solve the linear system and do postprocessing
     tangent, info = slg.lgmres(sys, rhs, x0=prev_tangent, maxiter=min(M+2, 10))
-    #res = lg.norm(sys(tangent) - rhs)
-    #print('LGMRES Residual', res)
+    tangent_residual = lg.norm(sys(tangent) - rhs)
+    if tangent_residual > 0.01:
+        # Solve the linear system using Newton-Krylov with much better lgmres arguments
+        def F(v):
+            return matvec(v) - rhs
+        tangent = opt.newton_krylov(F, prev_tangent, rdiff=rdiff, verbose=False)
+
+    # Make sure the new tangent lies in the direction of the previous one and return
     tangent = np.sign(np.dot(tangent, prev_tangent)) * tangent / lg.norm(tangent)
     return tangent
