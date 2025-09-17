c9974c4542199797c465f1803d49857c43795ad0
Hannes Vandecasteele <hannesvdc@gmail.com>
2025-09-17 17:33:44 +0200

    Added docstring to bifurcation.py functions.

--- src/pycont/Bifurcation.py
+++ src/pycont/Bifurcation.py
@@ -11,6 +11,37 @@ def test_fn_jacobian(F : Callable[[np.ndarray], np.ndarray],
 					 M : int, 
 					 w_prev : np.ndarray, 
 					 sp : Dict) -> Tuple[np.ndarray, float]:
+    """
+    Bifurcation point test function. A bifurcation point is given by the scalar 
+    `beta = 1/ l^T w' changing sign, where `w` is the solution to `J w = r` with
+    `J` the Jacobian of the extended objective function `F`. 
+
+    The Jacobian system is first solved using L-GMRES and refined with newton_krylov
+    when the initial residual is too large.
+
+    Parameters
+    ----------
+    F : Callable
+        The extended objective function.
+    x : ndarray
+        The current point `(u,p)` on the branch.
+    l, r : ndarray 
+        The left and right test vectors. Cannot have any components in the direction of 
+        the current tangent. Must also be normalized.
+    M : int
+        The dimension of the state vector `u`.
+    w_prev : ndarray
+        Solution to the Jacobian system in the previous point on the branch. Used as initial guess.
+    sp : Dict
+        Solver parameters.
+
+    Returns
+    -------
+        w_solution : ndarray
+            The full solution to the Jacobian system.
+        beta : float
+            The value of the test function. Monitor this for sign changes.
+    """
     rdiff = sp["rdiff"]
     maxiter = M if M < 10 else 10
     def matvec(w):
@@ -42,6 +73,36 @@ def computeBifurcationPoint(F : Callable[[np.ndarray], np.ndarray],
                             w : np.ndarray,
                             M : int,
 							sp : Dict) -> Tuple[bool, np.ndarray, float]:
+    """
+    Function that localizes the bifurcation point, if any, within a given tolerance.
+
+    Parameters
+    ----------
+    F : Callable
+        The extended objective function.
+    x_start : ndarray
+        A point on the branch before the bifurcation point.
+    x_end : ndarray
+        A point on the branch after the bifurcation point.
+    l, r : ndarray 
+        The left and right test vectors.
+    w : ndarray
+        Solution to the Jacobian system at x_start. Used as initial guess.
+    M : int
+        The dimension of the state vector `u`.
+    sp : Dict
+        Solver parameters.
+
+    Returns
+    -------
+        is_bf_point : bool
+            True if we actually found a bifurcation point, False otherwise. Latter happens when the brentq
+            optimizer did not converge.
+        x_singular : ndarray
+            The location of the bifurcation point, if converged.
+        alpha_singular : float
+            Fraction of the arc length between x_start and x_end where the bifurcation point lies.
+    """
     rdiff = sp["rdiff"]
     x_diff = x_end - x_start
     S = np.dot(l, w)
