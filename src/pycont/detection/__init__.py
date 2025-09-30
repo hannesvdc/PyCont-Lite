598b7025b85853872ba61b069dfbea3565630e81
Hannes Vandecasteele <hannesvdc@gmail.com>
2025-09-30 11:33:32 +0200

    Implemented DectionModule base class in detection/

---
+++ src/pycont/detection/__init__.py
@@ -0,0 +1,12 @@
+# src/pycont/detection/__init__.py
+from .base import DetectionModule
+
+# Re-export detectors you actually ship. Use try/except if some are optional.
+# from .hopf import HopfDetector
+# from .fold import FoldDetector
+# from .bp import BranchPointDetector
+# from .param_limits import ParamLimitDetector
+
+__all__ = [
+    "DetectionModule",
+]
---
+++ src/pycont/detection/base.py
@@ -0,0 +1,72 @@
+import numpy as np
+
+import abc
+from typing import Callable, Dict, Any
+
+ObjectiveType = Callable[[np.ndarray, float], np.ndarray]
+
+class DetectionModule(abc.ABC):
+
+    def __init__(self):
+        pass
+
+    def initializeBranch(self,
+                         G : ObjectiveType,
+                         x : np.ndarray,
+                         tangent : np.ndarray,
+                         sp : Dict[str, Any]) -> None:
+        """
+        Initialize detection on a new branch. This function should reset all fields
+        within the DetectionModule subclass.
+
+        Parameters
+        ----------
+        G : Callable
+            The continuation objective function.
+        x : ndarray
+            The initial point on the branch
+        tangent : ndarray
+            The tangent to the branch at the inital point.
+        sp : Optional Dict
+            The solver parameters.
+
+        Returns
+        -------
+        Nothing.
+        """
+        self.G = G
+        self.sp = {} if sp is None else dict(sp)
+
+    @abc.abstractmethod
+    def update(self,
+               x_new : np.ndarray,
+               tangent_new : np.ndarray) -> bool:
+        """
+        Update the detection module with a new point on the branch. Returns true if
+        a special point was passed, False otherwise.
+
+        Parameters
+        ----------
+        x_new : ndarray
+            The new point on the branch.
+        tangent_new : ndarray
+            Tangent at the new point.
+
+        Returns
+        -------
+        passed_point : bool
+            True if a special point was passed, False otherwise.
+        """
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def localize(self) -> np.ndarray:
+        """
+        Localize the special point to high precision.
+
+        Returns
+        -------
+        x_loc : ndarray
+            Point on the branch where the detection function is exactly 0, up to the tolerance.
+        """
+        raise NotImplementedError
