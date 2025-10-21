55817c6c3d2005559590158aac407d0d64b313ad
Hannes Vandecasteele <hannesvdc@gmail.com>
2025-10-20 21:59:04 -0400

    Added limit cycle plotting capabilities to pycont-lite.

--- src/pycont/plotting.py
+++ src/pycont/plotting.py
@@ -1,10 +1,16 @@
 import numpy as np
 import numpy.linalg as lg
 import matplotlib.pyplot as plt
+from matplotlib.colors import Normalize
+from matplotlib.cm import ScalarMappable
 
 from .Types import ContinuationResult
 
 def plotBifurcationDiagram(cr : ContinuationResult, **kwargs) -> None:
+    if len(cr.branches) == 0:
+        print('No active branches to plot. Returning')
+        return
+    M = cr.branches[0].u_path.shape[1]
     
     if "p_label" in kwargs:
         xlabel = kwargs["p_label"]
@@ -27,22 +33,67 @@ def plotBifurcationDiagram(cr : ContinuationResult, **kwargs) -> None:
             u_transform = lambda u : lg.norm(u)
             ylabel = r'$||u||$'
 
-    # Plot the branches
+    # Plot the regular branches first
+    bf_fig, bf_ax = plt.subplots()
     for branch in cr.branches:
-        u_vals = np.apply_along_axis(u_transform, 1, branch.u_path)
-        linestyle = '--' if not branch.stable else '-'
-        plt.plot(branch.p_path, u_vals, color='tab:blue', linestyle=linestyle)
+        if branch.is_lc:
+            # Plot u(t)[0] versus u(t)[1]
+            npoints = branch.u_path.shape[0]
+            L = (branch.u_path.shape[1]-1) // M
+            Q_points = np.reshape(branch.u_path[:,:-1], (npoints, M, L))
+            p_values = branch.p_path
+            branch.from_event
+
+            if branch.from_event is not None:
+                hopf_location = cr.events[int(branch.from_event)].u
+            else:
+                hopf_location = None
+            _plotLimitCycleFamily(Q_points, p_values, hopf_location)
+
+        else:
+            u_vals = np.apply_along_axis(u_transform, 1, branch.u_path)
+            linestyle = '--' if not branch.stable else '-'
+            bf_ax.plot(branch.p_path, u_vals, color='tab:blue', linestyle=linestyle)
 		
     # Plot all interesting points
     style = {"SP" : 'go', "LP" : 'bo', "BP" : 'ro', "HB": 'mo', "DSFLOOR": 'co'}
     for event in cr.events:
         if event.kind in style.keys():
-            plt.plot(event.p, u_transform(event.u), style[event.kind], label=event.kind)
-    handles, labels = plt.gca().get_legend_handles_labels()
+            bf_ax.plot(event.p, u_transform(event.u), style[event.kind], label=event.kind)
+    handles, labels = bf_ax.get_legend_handles_labels()
     by_label = dict(zip(labels, handles))
-    plt.legend(by_label.values(), by_label.keys(), loc='best')
+    bf_ax.legend(by_label.values(), by_label.keys(), loc='best')
 
-    plt.grid(visible=True)
-    plt.xlabel(xlabel)
-    plt.ylabel(ylabel)
+    bf_ax.grid(visible=True)
+    bf_ax.set_xlabel(xlabel)
+    bf_ax.set_ylabel(ylabel)
+
     plt.show()	
+
+def _plotLimitCycleFamily(Q_points, p_values, u_hopf):
+    u0_t = Q_points[:,0,:]
+    u1_t = Q_points[:,1,:]
+
+    fig, ax = plt.subplots()
+    norm = Normalize(p_values.min(), p_values.max())
+    cmap = 'plasma'
+    cm = plt.get_cmap(cmap)
+
+    stride = 10
+    for k in range(0, u0_t.shape[0], stride):
+        x = u0_t[k]; y = u1_t[k]
+
+        # close the loop for plotting aesthetics
+        x_plot = np.r_[x, x[0]]; y_plot = np.r_[y, y[0]]
+        color = cm(norm(p_values[k]))
+        ax.plot(x_plot, y_plot, color=color)
+
+    # Add a colorbar
+    sm = ScalarMappable(norm=norm, cmap=cm)
+    cbar = fig.colorbar(sm, ax=ax)
+    cbar.set_label(r'$p$', rotation=0, labelpad=12, va='top')
+
+    ax.set_xlabel(r'$u_0(t)$')
+    ax.set_ylabel(r'$u_1(t)$')
+    if u_hopf is not None:
+        ax.set_title(rf'Limit Cycles from Hopf Point $({u_hopf[0]}, {u_hopf[1]})$')
