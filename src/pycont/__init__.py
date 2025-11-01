05d59aa3f306ccd42effa8b81e00bfe8ca97becf
Hannes Vandecasteele <hannesvdc@gmail.com>
2025-11-01 11:45:27 -0400

    release: v0.6.0

--- src/pycont/__init__.py
+++ src/pycont/__init__.py
@@ -2,7 +2,7 @@ from .continuation import pseudoArclengthContinuation as arclengthContinuation
 from .plotting import plotBifurcationDiagram
 from .Logger import Verbosity
 
-__version__ = "0.5.0"
+__version__ = "0.6.0"
 
 __all__ = [
     "arclengthContinuation", 
